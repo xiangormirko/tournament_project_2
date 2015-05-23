9664c0f03000f7560e7e6463abdf891cbec7a029
xiangormirko <xiangormirko@gmail.com>
2015-05-23 10:06:45 +0200

    initial commit

---
+++ tournament.py
@@ -0,0 +1,144 @@
+#!/usr/bin/env python
+#
+# tournament.py -- implementation of a Swiss-system tournament
+#
+
+import psycopg2
+
+
+def connect():
+    """Connect to the PostgreSQL database.  Returns a database connection."""
+    return psycopg2.connect("dbname=tournament")
+
+
+def deleteMatches():
+    """Remove all the match records from the database."""
+    # standard database action
+    # connection, cursor, execute query, commit, close
+    DB = psycopg2.connect("dbname=tournament")
+    c = DB.cursor()
+    c.execute("DELETE FROM matches")
+    DB.commit()
+    DB.close()
+    print("Match records have been erased")
+
+
+def deletePlayers():
+    """Remove all the player records from the database."""
+    DB = psycopg2.connect("dbname=tournament")
+    c = DB.cursor()
+    c.execute("DELETE FROM players")
+    DB.commit()
+    DB.close()
+    print("Player records have been erased")
+
+
+def countPlayers():
+    """Returns the number of players currently registered."""
+    DB = psycopg2.connect("dbname=tournament")
+    c = DB.cursor()
+    c.execute("SELECT COUNT(*) FROM players")
+    players = c.fetchall()[0][0]
+    DB.close()
+    return players
+
+
+def registerPlayer(name):
+    """Adds a player to the tournament database.
+
+    The database assigns a unique serial id number for the player.  (This
+    should be handled by your SQL database schema, not in your Python code.)
+
+    Args:
+      name: the player's full name (need not be unique).
+    """
+    # takes in a name and registers a player
+    DB = psycopg2.connect("dbname=tournament")
+    c = DB.cursor()
+    c.execute("INSERT INTO players (name) VALUES (%s)", (name,))
+    DB.commit()
+    DB.close()
+    print("A new player named {} joined the tournament".format(name))
+
+
+def playerStandings():
+    """Returns a list of the players and their win records, sorted by wins.
+
+    The first entry in the list should be the player in first place,
+    or a player tied for first place if there is currently a tie.
+
+    Returns:
+      A list of tuples, each of which contains (id, name, wins, matches):
+        id: the player's unique id (assigned by the database)
+        name: the player's full name (as registered)
+        wins: the number of matches the player has won
+        matches: the number of matches the player has played
+    """
+    # fetches from a view called standings
+
+    DB = psycopg2.connect("dbname=tournament")
+    c = DB.cursor()
+    c.execute("SELECT * FROM standings;")
+    standing = []
+
+    for row in c.fetchall():
+        if row[3] == None:
+            standing.append((row[0], row[1], 0, 0))
+        else:
+            standing.append(row)
+    DB.close()
+
+    return standing
+
+
+def reportMatch(winner, loser):
+    """Records the outcome of a single match between two players.
+
+    Args:
+      winner:  the id number of the player who won
+      loser:  the id number of the player who lost
+    """
+    # registers a match result with winner and loser
+
+    DB = psycopg2.connect("dbname=tournament")
+    c = DB.cursor()
+    c.execute("INSERT INTO matches VALUES (%s, %s)", (winner, loser))
+    DB.commit()
+    DB.close()
+    print("A match data has been recorded")
+
+
+def swissPairings():
+    """Returns a list of pairs of players for the next round of a match.
+
+    Assuming that there are an even number of players registered, each player
+    appears exactly once in the pairings.  Each player is paired with another
+    player with an equal or nearly-equal win record, that is, a player adjacent
+    to him or her in the standings.
+
+    Returns:
+      A list of tuples, each of which contains (id1, name1, id2, name2)
+        id1: the first player's unique id
+        name1: the first player's name
+        id2: the second player's unique id
+        name2: the second player's name
+    """
+
+    # the view in the database is organized with id, name
+    # ordered by amount of winnings
+    DB = psycopg2.connect("dbname=tournament")
+    c = DB.cursor()
+    c.execute("SELECT * FROM pairings;")
+    standings = c.fetchall()
+    pairings = []
+    i = 0
+    # the code goes through ever 2 pair and appends them
+    while i < len(standings):
+        player1 = standings[i]
+        player2 = standings[i+1]
+        pair = player1+player2
+        pairings.append(pair)
+        i += 2
+    DB.close()
+
+    return pairings
---
+++ tournament.sql
@@ -0,0 +1,48 @@
+-- Table definitions for the tournament project.
+--
+-- Put your SQL 'create table' statements in this file; also 'create view'
+-- statements if you choose to use it.
+--
+-- You can write comments in this file by starting them with two dashes, like
+-- these lines here.
+
+CREATE TABLE players ( name TEXT,
+                     id SERIAL PRIMARY KEY );
+
+
+CREATE TABLE matches ( winner INT references players (id),
+                     loser INT references players (id));
+
+--A view named wins to display wins grouped by player and ordered by wins
+CREATE VIEW wins AS 
+SELECT p.id, p.name, COUNT(m.winner) 
+AS winnings FROM players p 
+LEFT JOIN matches m  
+ON p.id = m.winner 
+GROUP BY p.id 
+ORDER BY winnings DESC;
+
+--A view named count to display total number of matches played by player
+CREATE VIEW count AS 
+SELECT players.id, players.name, COUNT(matches) 
+AS total_matches FROM players 
+LEFT JOIN matches 
+ON players.id = winner OR players.id = loser 
+GROUP BY players.id ORDER BY players.id;  
+
+--A view named standings to display current standings of players
+CREATE VIEW standings 
+AS SELECT players.id, players.name, wins.winnings, count.total_matches 
+FROM players, wins, count 
+WHERE players.id = wins.id AND wins.id = count.id 
+ORDER BY wins.winnings DESC;
+
+--A simple view to facilitate the pairing of players
+CREATE VIEW pairings AS 
+SELECT standings.id, standings.name 
+FROM standings; 
+
+
+
+
+
